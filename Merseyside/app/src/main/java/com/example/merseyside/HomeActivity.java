cb9c9b06a293b7c61c64b8df5f246824769d8436
unknown <daniel.dimitrov.2021@gmail.com>
2019-03-15 20:24:10 +0200

    Menu added.

--- Merseyside/app/src/main/java/com/example/merseyside/HomeActivity.java
+++ Merseyside/app/src/main/java/com/example/merseyside/HomeActivity.java
@@ -3,10 +3,12 @@ package com.example.merseyside;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -77,8 +79,12 @@ public class HomeActivity extends AppCompatActivity {
 
             System.out.println("account: " + account.getEmail());
             System.out.println(account.getId());
+            System.out.println(account.getDisplayName());
+            System.out.println(account.getPhotoUrl());
 
-            Intent myIntent = new Intent(this, NewActivity.class);
+            Intent myIntent = new Intent(this, MenuActivity.class);
+            myIntent.putExtra("EMAIL", account.getEmail());
+            myIntent.putExtra("NAME", account.getDisplayName());
             this.startActivity(myIntent);
 
             // Signed in successfully, show authenticated UI.
